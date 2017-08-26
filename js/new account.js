18810a64ae7dc6f54d830ea2b69f07ac9a0c48ba
SteveSu55 <steve20130309@gmail.com>
2017-08-26 16:05:05 +0800

    Home

---
+++ js/new account.js
@@ -0,0 +1,27 @@
+var fdb = new ForerunnerDB();
+var db = fdb.db("my db");
+var accountCollection = db.collection('account');
+
+accountCollection.load();
+
+$("#submit").click(function(){
+	var date = $("#date").val();
+	var category = $("#category").val();
+	var item = $("#item").val();
+	var cost = $("#cost").val();
+
+	accountCollection.insert({
+		date : date,
+		category : category,
+		item : item,
+		cost : cost
+	});
+
+	accountCollection.save();
+	alert("Complete")
+
+	$("#date").val("");
+	$("#category").val("");
+	$("#item").val("");
+	$("#cost").val("");
+});
